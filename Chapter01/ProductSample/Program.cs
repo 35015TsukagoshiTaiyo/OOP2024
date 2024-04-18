9cc47ec5ba52f65890b50c50e5b1b8460546b71c
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-04-18 13:37:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using SampleApp;
+using System;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(124, "だいふく", 200);
+            Product dorayaki = new Product(98, "どらやき", 210);
+
+            int price = karinto.Price;  //  税抜きの金額
+            int taxIncluded = karinto.GetPriceIncludingTax();   //税込みの金額
+
+            int daifukuPrice = daifuku.Price;
+            int daifukuTaxIncluded = daifuku.GetPriceIncludingTax();
+
+
+            int dorayakiTax = dorayaki.GetTax();
+
+            Console.WriteLine(karinto.Name + "の税込金額"
+                                        + taxIncluded + "円【税抜き" + price + "円】");
+            Console.WriteLine(daifuku.Name + "の税込金額"
+                                        + daifukuTaxIncluded + "円【税抜き" + daifukuPrice + "円】");
+
+            Console.WriteLine(dorayaki.Name + "の消費税額" + dorayakiTax + "円");
+            Console.WriteLine($"{dorayakiTax}円");
+        }
+    }
+}
