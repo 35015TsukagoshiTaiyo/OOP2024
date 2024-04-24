af890c6c68f489e7142a90f1d40ffd9f72da9382
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-04-24 15:46:49 +0900

    問題2.1.4完成

--- Chapter02/演習問題/Exercise1/Program.cs
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -11,9 +11,17 @@ namespace Exercise1 {
             var songs = new Song[] {
                 new Song("アイドル","YOASOBI",226),
                 new Song("夜に駆ける","YOASOBI",262),
-                new Song("群青","YOASOBI",263),           
+                new Song("群青","YOASOBI",263),
             };
+            PrintSongs(songs);
+        }
 
+        private static void PrintSongs(Song[] songs) {
+
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0} {1} {2:mm\:ss}",
+                                        song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
         }
     }
 }
