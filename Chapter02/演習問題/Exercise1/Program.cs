028f4c633c800305bab2aa8d34a3643fb715a737
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-04-24 15:39:32 +0900

    問題2.2.3完成

--- Chapter02/演習問題/Exercise1/Program.cs
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     internal class Program {
         static void Main(string[] args) {
+
+            var songs = new Song[] {
+                new Song("アイドル","YOASOBI",226),
+                new Song("夜に駆ける","YOASOBI",262),
+                new Song("群青","YOASOBI",263),           
+            };
+
         }
     }
 }
