a21b60931768c868f48530364c820c97a3422d43
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-05-15 14:24:34 +0900

    問題3.1　完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -7,6 +7,36 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+
+            // 3.1.2
+            Exercise1_2(numbers);
+
+            // 3.1.3
+            Exercise1_3(numbers);
+
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exits = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            Console.WriteLine(exits);
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+
         }
     }
 }
