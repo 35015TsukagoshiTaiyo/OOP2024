4349a4da3c6d5639c4e373519d84542d73982e9d
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-05-22 11:44:52 +0900

    演習問題02　模範解答

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -55,9 +55,9 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_4(List<string> names) {
-            var selected = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
-            foreach (var name in selected) { 
-                Console.WriteLine(name);
+            var selected = names.Where(s => s.StartsWith("B")).Select(s => new { s.Length, s });
+            foreach (var obj in selected) {
+                Console.WriteLine(obj.s + "," + obj.Length);
             }
         }
     }
