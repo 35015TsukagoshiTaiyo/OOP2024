217d8607cc7a8cfeef752e7974bfc34838c1f34e
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-05-22 11:33:21 +0900

    途中から3.2.4　p100

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -12,28 +12,53 @@ namespace Exercise02 {
                 "Paris", "Berlin", "Canberra", "Hong Kong",
             };
 
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_1(names);
             Console.WriteLine();
 
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_2(names);
             Console.WriteLine();
 
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_3(names);
             Console.WriteLine();
 
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_4(names);
+            Console.WriteLine();
         }
 
         private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力。空行で終了");
+            do {
+                //入力処理
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break; //空行だったら抜ける
+
+                int index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
         }
 
         private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<string> names) {
+            var selected = names.Where(s => s.Contains('o')).ToArray();
+            foreach (var name in selected) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise2_4(List<string> names) {
+            var selected = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
+            foreach (var name in selected) { 
+                Console.WriteLine(name);
+            }
         }
     }
 }
