bbb582b5e13e96a6dfbd4c05b5dbb49349682c5f
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-05-29 15:44:52 +0900

    5.1 5.2 完成

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("文字を入力：");
+            var str1 = Console.ReadLine();
+            Console.Write("文字を入力：");
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("数字を入力：");
+            var str = Console.ReadLine();
+            int num;
+
+            if (int.TryParse(str, out num)) {
+                string numStr = String.Format("{0:N0}", num);
+                Console.WriteLine(numStr);
+            }
+            
+            
+
+        }
+    }
+}
