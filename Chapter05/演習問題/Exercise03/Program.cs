26627631f5b0b9ee85a6c1342d359c2209814a92
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-05-29 15:49:04 +0900

    5.3.1 完成

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -25,23 +25,24 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_1(string text) {
-           
+            int spaceCount = text.Where(c => c == ' ').Count();
+            Console.WriteLine("空白の数：" + spaceCount);
         }
 
         private static void Exercise3_2(string text) {
-        
+
         }
 
         private static void Exercise3_3(string text) {
-        
+
         }
 
         private static void Exercise3_4(string text) {
-        
+
         }
 
         private static void Exercise3_5(string text) {
-        
+
         }
     }
 }
