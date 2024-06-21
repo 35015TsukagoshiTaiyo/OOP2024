0e80fea08ce6302987c6dc905f6c782223b0217e
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-06-21 10:16:32 +0900

    自分の生年月日の曜日表示

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            #region p204
+            //var dt1 = new DateTime(2024, 6, 19);
+            //var dt2 = new DateTime(2010, 3, 12, 8, 45, 20);
+            //Console.WriteLine(dt1);
+            //Console.WriteLine(dt2);
+
+
+            //var today = DateTime.Today;
+            //var now = DateTime.Now;
+            //Console.WriteLine("Today : {0}", today);
+            //Console.WriteLine("Now : {0}", now);
+
+            //var isLeapYear = DateTime.IsLeapYear(2024);
+            //if (isLeapYear) {
+            //    Console.WriteLine("閏年です");
+            //} else {
+            //    Console.WriteLine("閏年ではありません");
+            //}
+            #endregion
+
+            //var dt1 = new DateTime(2004, 7, 22);
+            //DayOfWeek dayOfWeek = dt1.DayOfWeek;
+            //Console.WriteLine(dayOfWeek);
+
+            Console.WriteLine("生年月日を入力");
+            Console.Write("年：");
+            var year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            var month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            var day = int.Parse(Console.ReadLine());
+
+            var dt1 = new DateTime(year, month, day);
+
+            CultureInfo culture = new CultureInfo("ja-JP");
+            Console.WriteLine("あなたは{0}に生まれました。", dt1.ToString("dddd", culture));
+
+
+
+        }
+    }
+}
