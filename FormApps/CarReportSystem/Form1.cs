5df240523825058ee270a0143d953e303558ba50
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-07-12 11:52:59 +0900

    シングルトン実装

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -12,7 +12,7 @@ namespace CarReportSystem {
         BindingList<CarReport> listCarReports = new BindingList<CarReport>();
 
         //設定のインスタンス生成
-        Settings settings = new Settings();
+        Settings settings = Settings.getInstance();
 
         //コンストラクタ
         public Form1() {
