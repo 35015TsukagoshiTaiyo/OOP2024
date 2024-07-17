2a5f5180fe4d9fb709d2e75bf354a8eafcbd94c5
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-07-17 15:19:17 +0900

    問題11.1-1 完成

--- Chapter11/演習問題/Exercise01/Program.cs
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+
+
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements().Select(x => new {
+                Name = (string)x.Element("name"),
+                teamMembers = (int)x.Element("teammembers"),
+            });
+            
+            foreach (var xelement in xelements) {
+                Console.WriteLine("{0} {1}", xelement.Name, xelement.teamMembers);
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+
+        }
+
+        private static void Exercise1_3(string file) {
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+
         }
     }
 }
