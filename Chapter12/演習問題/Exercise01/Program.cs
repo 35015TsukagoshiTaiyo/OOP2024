a5f5ea1f7d9ae69463d88c839cdab57e83d9e6e0
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-08-27 11:07:52 +0900

    演習問題 12.1.1 完成

---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+
+
+        }
+
+        private static void Exercise1_1(string outfile) {
+            //シリアル化
+            var employee = new Employee {
+                Id = 1,
+                Name = "佐藤",
+                HireDate = DateTime.Today,
+            };
+            using (var writer = XmlWriter.Create(outfile)) {
+                var serializer = new XmlSerializer(employee.GetType());
+                serializer.Serialize(writer, employee);
+            }
+
+            //逆シリアル化
+            using (var reader = XmlReader.Create(outfile)) {
+                var serializer = new XmlSerializer(typeof(Employee));
+                var emp = serializer.Deserialize(reader) as Employee;
+                Console.WriteLine(emp);
+            }
+        }
+
+        private static void Exercise1_2(string outfile) {
+
+        }
+
+        private static void Exercise1_3(string v) {
+
+        }
+
+        private static void Exercise1_4(string v) {
+
+        }
+    }
+}
