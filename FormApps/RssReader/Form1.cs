dba550a701ef3d00af79da5afaaa52e6e3f29737
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-09-03 11:13:15 +0900

    title 表示

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+
+
+                var items = xdoc.Descendants("item");
+                foreach ( var item in items ) {
+                    var title = item.Element("title").Value;
+                    lbRssTitle.Items.Add(title);
+                }
+                
+
+
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
+            
+        }
+    }
+}
