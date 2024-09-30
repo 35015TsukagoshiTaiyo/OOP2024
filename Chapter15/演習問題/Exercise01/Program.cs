1ade349fc481cc37b1d92333ce868f017abf2ff7
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-09-30 15:47:02 +0900

    演習問題 15.1.5 15.1.6 完成 4修正 3模範解答

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,29 +32,69 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_3() {
+#if true
             var groups = Library.Books
                 .GroupBy(b => b.PublishedYear)
                 .OrderBy(g => g.Key);
             foreach (var group in groups) {
                 Console.WriteLine("{0}年 {1}冊", group.Key, group.Count());
             }
+#else //模範解答
+            var query = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(g => new { publishedYear = g.Key, count = g.Count() })
+                .OrderBy(g => g.publishedYear);
+                foreach (var group in groups) {
+                Console.WriteLine("{0}年 {1}冊", group.Key, group.Count());
+            }
+#endif
         }
 
         private static void Exercise1_4() {
             var books = Library.Books
                 .OrderByDescending(b => b.PublishedYear)
-                .ThenByDescending(b => b.Price);
+                .ThenByDescending(b => b.Price)
+                .Join(Library.Categories, //結合する２番目のシーケンス
+                        book => book.CategoryId, //対象シーケンスの結合キー
+                        category => category.Id, //２番目のシーケンスの結合キー
+                        (book, category) => new { //オブジェクト生成関数
+                            book.PublishedYear,
+                            book.Price,
+                            book.Title,
+                            CategoryName =category.Name,
+                        }
+                    );
             foreach (var book in books) {
-                Console.WriteLine(book);
+                Console.WriteLine($"{book.PublishedYear}年 {book.Price}円 {book.Title} ({book.CategoryName})  ");
             }
         }
 
         private static void Exercise1_5() {
-
+            var names = Library.Books
+                .Where(b => b.PublishedYear == 2016)
+                .Join(Library.Categories,
+                        book => book.CategoryId,
+                        category => category.Id,
+                        (book, category) => category.Name)
+                .Distinct();
+            foreach (var name in names) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise1_6() {
-
+            var groups = Library.Categories
+                .OrderBy(c=> c.Name)
+                .GroupJoin(Library.Books,
+                            c => c.Id,
+                            d => d.CategoryId,
+                            (c, books) => new { category = c.Name, Books = books });
+            foreach (var group in groups) {
+                Console.WriteLine(group.category);
+                foreach (var book in group.Books) {
+                    Console.WriteLine($"  {book.Title}");
+                }
+            }
         }
 
         private static void Exercise1_7() {
