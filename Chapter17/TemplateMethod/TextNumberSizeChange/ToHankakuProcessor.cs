f7dba8e37a24f24d3b00cbdad7855eea38f5ecb2
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-10-10 12:07:55 +0900

    Excuteのみ

--- Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
+++ Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
@@ -9,25 +9,16 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace TextNumberSizeChange {
     class ToHankakuProcessor : TextProcessor {
-        private int _count;
+        
         Dictionary<char, char> numDictionary = new Dictionary<char, char>() {
                 {'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},
                 {'６','6'},{'７','7'},{'８','8'},{'９','9'},{'０','0'},
         };
-
-        protected override void Initialize(string fname) {
-            _count = 0;
-        }
+        
         protected override void Execute(string line) {
             string s = new string(line.Select(n =>
                         (numDictionary.ContainsKey(n) ? numDictionary[n] : n)).ToArray());
-
             Console.WriteLine(s);
-            _count++;
-        }
-
-        protected override void Terminate() {
-            Console.WriteLine("{0}行", _count);
         }
     }
 }
