d7a66f822b47a34e639f98607a387595b3edd98f
TsukagoshiTaiyo <oibc35015@ojs.ac.jp>
2024-11-11 14:53:16 +0900

    ラジオボタン 赤黄緑作成

---
+++ WPF/VisibilityConverter/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisibilityConverter {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e) {
+            Resources["ButtonBrushKey"] = new SolidColorBrush(Colors.Red);
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e) {
+        //    Resources["ButtonBrushKey"] = new SolidColorBrush(Colors.Gray);
+        //}
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e) {
+            Resources["ButtonBrushKey"] = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e) {
+            Resources["ButtonBrushKey"] = new SolidColorBrush(Colors.Green);
+        }
+    }
+}
